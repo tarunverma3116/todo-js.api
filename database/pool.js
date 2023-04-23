83b8a793dc590aa391f9b9241aa30636e6de56c9
Tarun Verma <tarunverma@Taruns-MacBook-Air-2.local>
2023-04-23 22:24:41 +0530

    env config

--- database/pool.js
+++ database/pool.js
@@ -5,14 +5,14 @@ dotenv.config();
 const connectDB = () => {
   const pool = mysql
     .createPool({
-      // host: process.env.MYSQL_HOST,
-      // user: process.env.MYSQL_USER,
-      // password: process.env.MYSQL_PASSWORD,
-      // database: process.env.MYSQL_DATABASE,
-      host: "127.0.0.1",
-      user: "root",
-      password: "8102788088",
-      database: "todo_app",
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      // host: "127.0.0.1",
+      // user: "root",
+      // password: "8102788088",
+      // database: "todo_app",
     })
     .promise();
   return pool;
